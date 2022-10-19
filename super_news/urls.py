3f4726515baf419b13d3a0f264b80e201c386aee
sergyanov <info@paniyanovska.ua>
2022-10-19 22:55:34 +0300

    [lesson_05] | change class ORM | creation of test articles, authors, categories in the site admin panel | connecting WYSIWYG editor - django-summernote

--- super_news/urls.py
+++ super_news/urls.py
@@ -32,8 +32,11 @@ urlpatterns = [
 
     path('robots.txt', views.robots_hendler),
 
+    path('summernote/', include('django_summernote.urls')),
     path('admin/', admin.site.urls),
 
-    path('__debug__/', include('debug_toolbar.urls')),
-
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [path('__debug__/', include('debug_toolbar.urls'))]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
