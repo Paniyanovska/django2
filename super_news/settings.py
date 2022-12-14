965aa10fea5ac410570c96c7c7ece52c9ad14ab4
sergyanov <info@paniyanovska.ua>
2022-12-14 19:36:31 +0200

    add settings local

--- super_news/settings.py
+++ super_news/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = [BASE_DIR / "static_root"]
+
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "/media/"
